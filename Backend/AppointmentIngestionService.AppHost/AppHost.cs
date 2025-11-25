91c9cdcaf2e04f94308a89b72f6bc92dc901ff4e
Meester Stefan <stefan.billiet@corilus.be>
2025-11-25 14:30:45 +0100

    Move backend code to Backend

--- Backend/AppointmentIngestionService.AppHost/AppHost.cs
+++ Backend/AppointmentIngestionService.AppHost/AppHost.cs
@@ -4,7 +4,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<WebApi>("api");
 
-var web = builder.AddViteApp("web", "../Frontend")
+var web = builder.AddViteApp("web", "../../Frontend")
     .WithExternalHttpEndpoints()
     .WithEnvironment("VITE_API_BASE_URL", api.GetEndpoint("http"))
     .WithReference(api)
